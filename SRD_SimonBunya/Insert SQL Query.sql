52cec8fad75a3ad9ad92cce2a5b16fec8676dd24
Simon <simonbunya@gmail.com>
2024-09-06 13:03:11 +0200

    New database scripts updated

---
+++ SRD_SimonBunya/Insert SQL Query.sql
@@ -0,0 +1,5 @@
+USE [SRD_SimonBunya]
+GO
+
+INSERT [dbo].[Forms] ([ID] ,[Title] ,[Key] ,[Tags] ,[CreatedDate] ,[LastUpdateDate],[Status]) 
+VALUES (1, 'New Emergency Form Title', 'New Emergency Form Keys' , 'New Emergency Form Tags', '2024-09-06' ,'2024-09-06' ,'Disabled')
